02bb0e87540ea6c69972c7292f114f88c4f41b9e
Adrian <adrianibarsam@gmail.com>
2020-06-13 19:03:27 -0500

    segundo commit Adrian Ibarra

---
+++ Assets/Scenes/Razas.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Razas : MonoBehaviour
+{
+    public GameObject[] races;
+    private int currentRace;
+
+    public void SwitchRaces()
+    {
+        if(currentRace == races.Length - 1)
+        {
+            currentRace = 0;
+        } else
+        {
+            currentRace++;
+        }
+
+        currentRace++;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < races.Length; i++)
+        {
+            if(i == currentRace)
+            {
+                races[i].SetActive(true);
+            } else
+            {
+                races[i].SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Tamaños.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tamaños : MonoBehaviour
+{
+    public GameObject[] Tamaño;
+    private int TamañoActual;
+
+    public void SwitchTamaños()
+    {
+        if (TamañoActual == Tamaño.Length - 1)
+        {
+            TamañoActual = 0;
+        }
+        else
+        {
+            TamañoActual++;
+        }
+
+        TamañoActual++;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Tamaño.Length; i++)
+        {
+            if (i == TamañoActual)
+            {
+                Tamaño[i].SetActive(true);
+            }
+            else
+            {
+                Tamaño[i].SetActive(false);
+            }
+        }
+    }
+}
